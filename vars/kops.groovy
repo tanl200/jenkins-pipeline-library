d61cb60e6736d0bbd93fa14eb2074845fe2e189f
Tan Luong <lhtan@fossil.com>
2018-04-12 01:11:00 +0700

    add kops script

--- vars/kops.groovy
+++ vars/kops.groovy
@@ -7,18 +7,25 @@ def call(body) {
     body.delegate = parameters
     body()
 
+    def status = 1 
+    def output = ''
+
     if (parameters.action=='create') {
-    	def status = sh(returnStatus: true, script: "echo create > create.log")
-    	return status
+    	status = sh(returnStatus: true, script: "echo create > create.log")
+    	output = readFile('create.log').trim()
     }
 
     if (parameters.action=='replace') {
-    	def status = sh(returnStatus: true, script: "echo replace > replace.log")
-    	return status
+    	status = sh(returnStatus: true, script: "echo replace > replace.log")
+    	output = readFile('create.replace').trim()
     }
 
     if (parameters.action!='create' && parameters.action!='replace') {
-    	def status = sh(returnStatus: true, script: "echo unknown > unknown.log")
-    	return 1
+    	output = 'unknown'
+    }
+
+    if ( status == 0) {
+    	sh "echo ${output}"
+    	currentBuild.result = 'FAILED'
     }
 }
