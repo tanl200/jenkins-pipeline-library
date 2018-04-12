e1eddd4b80e66ea7899efd9d3564c6ad80aa0532
Tan Luong <lhtan@fossil.com>
2018-04-12 14:01:47 +0700

    improve gitops

--- vars/gitops.groovy
+++ vars/gitops.groovy
@@ -11,7 +11,7 @@ def call(body) {
 
     sshagent (credentials: ['f1fe8468-e322-4b55-8599-0a3a6b79acbb']) {
         sh("git config --global user.email 'devops-bot@home.com'")
-        sh("git config --global user.name 'devops-bot'")
+        sh("git config --global user.name 'lhtan200'")
         sh("git checkout -b 'test-pr' ")
         sh("git add .")
         sh("git commit -m 'update'")
