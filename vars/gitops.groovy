28ddfb9beafe0340f80cd12fa24a79eccba6c10f
Tan Luong <lhtan@fossil.com>
2018-04-12 15:23:54 +0700

    improve gitops

--- vars/gitops.groovy
+++ vars/gitops.groovy
@@ -12,9 +12,9 @@ def call(body) {
     sshagent (credentials: ['f1fe8468-e322-4b55-8599-0a3a6b79acbb']) {
         sh("git config --global user.email ${parameters?.email}")
         sh("git config --global user.name ${parameters?.user}")
-        sh("git checkout -b auto-${parameters?.branch} ")
+        sh("git checkout -b auto-${BUILD_NUMBER} ")
         sh("git add .")
         sh("git commit -m 'auto-commit-${parameters?.commitMessage}'")
-        sh("git push origin auto-${parameters?.branch}")
+        sh("git push origin auto-${BUILD_NUMBER}")
     }
 }
