95250d526181d25c180d2147f48ac552262b6fe1
Tan Luong <lhtan@fossil.com>
2018-04-14 16:14:29 +0700

    kops functions attach file

--- vars/approve.groovy
+++ vars/approve.groovy
@@ -3,13 +3,20 @@
 def call(body) {
     // evaluate the body block, and collect configuration into the object
     def config = [:]
+    def output = ''
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
 
-    def proceedMessage = """${JOB_NAME} - ${BUILD_NUMBER}: ${config?.message}"""
+    def proceedMessage = """${JOB_NAME} - ${BUILD_NUMBER}: ${config?.message} via ${BUILD_URL}"""
 
-    slackSend channel: "#${config.slackChannel ?: builds}", message: proceedMessage
+    if (config.slackFile != "" ) {
+        output = readFile(config.slackFile).trim()    
+    } else {
+        output = 'empty'
+    }
+    
+    slackSend channel: "#${config.slackChannel ?: builds}", message: proceedMessage, attachments: output
 //    sh "echo ${proceedMessage}"
 
     timeout(time: config.timeout ?: 5, unit: config.timeUnit ?: "DAYS" ) {
