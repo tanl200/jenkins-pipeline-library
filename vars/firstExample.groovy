b18f4b02343bacd199fdeebce079781855523d05
Tan Luong <lhtan@fossil.com>
2018-04-16 02:53:53 +0700

    test parameters

--- vars/firstExample.groovy
+++ vars/firstExample.groovy
@@ -1,7 +1,11 @@
 #!/usr/bin/groovy
 
-def call(action, text2) {
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
 
-	println "${action}"
-	println "${text2}"
+    println config.action
+    println config.text2
 }
