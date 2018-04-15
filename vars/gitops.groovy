317479ce8bcde79fa730fbd879604dac929ea290
Tan Luong <lhtan@fossil.com>
2018-04-15 23:22:29 +0700

    kops functions attach file

--- vars/gitops.groovy
+++ vars/gitops.groovy
@@ -9,12 +9,22 @@ def call(body) {
     def status = 1 
     def output = ''
 
-    sshagent (credentials: ['f1fe8468-e322-4b55-8599-0a3a6b79acbb']) {
-        sh("git config --global user.email ${parameters?.email}")
-        sh("git config --global user.name ${parameters?.user}")
-        sh("git checkout -b auto-${BUILD_NUMBER} ")
-        sh("git add .")
-        sh("git commit -m 'auto-commit-${parameters?.commitMessage}'")
-        sh("git push origin auto-${BUILD_NUMBER}")
+    if (parameters.action=='push') {
+        sshagent (credentials: ['f1fe8468-e322-4b55-8599-0a3a6b79acbb']) {
+            sh("git config --global user.email ${parameters?.email}")
+            sh("git config --global user.name ${parameters?.user}")
+            sh("git checkout -b auto-${BUILD_NUMBER} ")
+            sh("git add .")
+            sh("git commit -m 'auto-commit-${parameters?.commitMessage}'")
+            sh("git push origin auto-${BUILD_NUMBER}")
+        }        
     }
+
+    if (parameters.action=='pull') {
+        sshagent (credentials: ['f1fe8468-e322-4b55-8599-0a3a6b79acbb']) {
+            sh("git checkout -b ${parameters.branch} ")
+            sh("git pull")
+        }
+    }
+
 }
