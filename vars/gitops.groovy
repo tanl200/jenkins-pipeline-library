4dbae61b0e2d2fad32268100432dc7da9287af12
Tan Luong <lhtan@fossil.com>
2018-04-16 08:13:54 +0700

    update

--- vars/gitops.groovy
+++ vars/gitops.groovy
@@ -23,7 +23,7 @@ def call(body) {
             sshagent (credentials: ['f1fe8468-e322-4b55-8599-0a3a6b79acbb']) {
                 sh("git config --global user.email ${parameters?.email}")
                 sh("git config --global user.name ${parameters?.user}")
-                sh("git checkout -b auto-${BUILD_NUMBER} ")
+                sh("git checkout -b auto-${BUILD_NUMBER}-${parameters.branch_suffix ?: info }")
                 sh("git add .")
                 sh("git commit -m 'auto-commit-${parameters?.commitMessage}'")
                 sh("git push origin auto-${BUILD_NUMBER}-${parameters.branch_suffix ?: info }")                
