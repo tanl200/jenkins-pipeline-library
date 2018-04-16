1d455b23e7df33c7b7ad1a1150e95d7af4d30bae
Tan Luong <lhtan@fossil.com>
2018-04-16 19:23:43 +0700

    test

--- vars/approve.groovy
+++ vars/approve.groovy
@@ -9,13 +9,13 @@ def call(body) {
 
     def proceedMessage = """${JOB_NAME} - ${BUILD_NUMBER} Request for Approve: ${config?.message} via ${BUILD_URL}"""
     
-    // slackSend channel: "#${config.slackChannel ?: builds}", message: proceedMessage
+    slackSend channel: "#${config.slackChannel ?: builds}", message: proceedMessage
 
-    notify {
+/*    notify {
         slackChannel: "#${config.slackChannel ?: builds}"
         message: proceedMessage
     }
-
+*/
     timeout(time: config.timeout ?: 5, unit: config.timeUnit ?: "DAYS" ) {
         input message: "${proceedMessage}", submitter: config.approveUser    
     }
