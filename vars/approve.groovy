874be9fdbe9884cbde0845b67a2aaff2a36930c0
Tan Luong <lhtan@fossil.com>
2018-04-16 17:26:14 +0700

    add notify script

--- vars/approve.groovy
+++ vars/approve.groovy
@@ -2,15 +2,21 @@
 def call(body) {
     // evaluate the body block, and collect configuration into the object
     def config = [:]
-    
+
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
 
     def proceedMessage = """${JOB_NAME} - ${BUILD_NUMBER}: ${config?.message} via ${BUILD_URL}"""
     
-    slackSend channel: "#${config.slackChannel ?: builds}", message: proceedMessage
-//    sh "echo ${proceedMessage}"
+    // slackSend channel: "#${config.slackChannel ?: builds}", message: proceedMessage
+
+    notify {
+        slackChannel: "#${config.slackChannel ?: builds}"
+        message: proceedMessage
+        title: "${JOB_NAME} - ${BUILD_NUMBER}: Request for Approve"
+        title_link: "${BUILD_NUMBER}"
+    }
 
     timeout(time: config.timeout ?: 5, unit: config.timeUnit ?: "DAYS" ) {
         input message: "${proceedMessage}", submitter: config.approveUser    
