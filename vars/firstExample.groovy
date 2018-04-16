fef4069d6dddbc9d3a22d27c787566a5a058bc8d
Tan Luong <lhtan@fossil.com>
2018-04-16 14:06:21 +0700

    test slacksend with attachments

--- vars/firstExample.groovy
+++ vars/firstExample.groovy
@@ -1,5 +1,9 @@
 #!/usr/bin/groovy
 
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+
 def call(body) {
     def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
@@ -8,4 +12,14 @@ def call(body) {
 
     println config.action2
     println config.text2
+
+	JSONObject attachment = new JSONObject();
+	attachment.put('text','I find your lack of faith disturbing!');
+	attachment.put('fallback','Hey, Vader seems to be mad at you.');
+	attachment.put('color','#ff0000');
+	JSONArray attachments = new JSONArray();
+	attachments.add(attachment);
+	println attachments.toString()
+
+    slackSend channel: "#k8s-build", message: proceedMessage, attachments: attachments.toString())
 }
