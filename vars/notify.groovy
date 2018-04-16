46e5402f5da9c1827f3cf44feebfc05f7a2c2648
Tan Luong <lhtan@fossil.com>
2018-04-16 18:40:26 +0700

    test

--- vars/notify.groovy
+++ vars/notify.groovy
@@ -29,11 +29,12 @@ def call(body) {
 	attachment.put('title_link',"${config?.title_link}");
 	attachments.add(attachment);
 */
+	String encodedUrl = URLEncoder.encode(url, "UTF-8");
 	attachment.put('text', "hello");
 	attachment.put('fallback', "moto");
 	attachment.put('color', color);
 	attachment.put('title',"okie");
-	attachment.put('title_link', URLEncoder.encode(config.title_link, "UTF-8"));
+	attachment.put('title_link', encodedUrl);
 	attachments.add(attachment)
     slackSend(color: 'good', channel: "#${config.slackChannel}", attachments: attachments.toString())
 }
